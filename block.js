0dbb80bb123d41fb8c99795d3f68907e5cb09063
HiyallFitsum <70286522+HiyallFitsum@users.noreply.github.com>
2020-10-07 09:59:01 -0400

    Add files via upload

---
+++ block.js
@@ -0,0 +1,33 @@
+class Block {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: false,
+          'restitution':0.5,
+            'friction':0,
+            'density':0.5
+      }
+      this.visibility = 225
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      if(this.body.speed <3){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill("red");
+      rect(0,0, this.width, this.height);
+      pop();
+    }else{
+      World.remove(world, this.body);
+      push()
+      this.visibility = this.visibility -5;
+      pop()
+        }
+    }
+  };
